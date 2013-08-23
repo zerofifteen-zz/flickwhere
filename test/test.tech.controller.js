bf5a54f3e486eb85778b4405cb9bd256c48462c3
Michael Vera <mvera@trapeze.com>
2013-08-23 09:07:12 -0400

    Removed test folder

--- test/test.tech.controller.js
+++
@@ -1,9 +0,0 @@
-/*jshint es5:true expr:true */
-
-suite('tech', function () {
-    var app = new Tech.Controller();
-
-    test('Instance is InstanceOf Tech', function () {
-        app.should.be.instanceof(Tech.Controller);
-    });
-});
